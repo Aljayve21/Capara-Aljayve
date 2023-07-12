24a9fbde5f83b4016eea6c7b2cd402240bb46096
Aljayve21 <aljayvecaparadc@gmail.com>
2023-07-12 14:17:37 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularproj';
+  title = 'Petzania Animal Clinic';
+  address = 'BGC Taguig';
+
+  constructor(private router: Router) {}
+
+  goHome() {
+    localStorage.clear();
+    this.router.navigate(['']);
+  }
 }
---
+++ src/app/nex-date.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NextDateDirective } from './nex-date.directive';
+
+describe('NextDateDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NextDateDirective();
+    expect(directive).toBeTruthy();
+  });
+});
